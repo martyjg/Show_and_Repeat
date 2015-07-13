fe74a1e45d74ec776889d59d502238799762069c
martyjg <martinjosephgormley@gmail.com>
2015-07-13 16:33:22 +0100

    Added buttons to progress the game. Added divs in order to style the game. Styled the game and aligned displays to the center and the player 1 character to the left side.

--- appV2.js
+++ appV2.js
@@ -5,36 +5,101 @@ $(document).ready(function () {
 	secondChain = [];
 	moveCount = 3;
 	turn = 0;
+	playerTurn = 0;
+	player1Score = 0;
+	player2Score = 0;
+	$("#p1Score").html(player1Score);
+	$("#p2Score").html(player2Score);
 
-	var states = [
-		"GAME_START",
-		"PLAYER_1_TURN",
-		"PLAYER_2_TURN",
-		"PLAYER_1_WIN",
-		"PLAYER_2_WIN"
-	];
+	// var states = [
+	// "GAME_START",
+	// "PLAYER_1_TURN",
+	// "PLAYER_2_TURN",
+	// "PLAYER_1_WIN",
+	// "PLAYER_2_WIN"
+	// ];
+
+	// showAndRepeat = function() {
+
+	// 	if (turn % 2 === 0) {
+	// 		chainBuilder(firstChain);
+	// 		if (firstChain.length === moveCount) {
+	// 			displayMoveChain(firstChain);
+	// 			clearDisplay();
+	// 			chainBuilder(secondChain);
+	// 			if (secondChain.length === moveCount) {
+	// 				checkForMatch();
+	// 			}
+	// 		}
+	// 	} else {
+	// 		chainBuilder(secondChain);
+	// 		if (secondChain.length === moveCount) {
+	// 			displayMoveChain(secondChain);
+	// 			clearDisplay();
+	// 			chainBuilder(firstChain);
+	// 			if (firstChain.length === moveCount) {
+	// 				checkForMatch();
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+	$("#checkMoves").click(function() {
+		checkForMatch();
+	})
+
+
+	$("#resetButton").click(function() {
+		if (turn % 2 === 0) {
+			if (playerTurn === 0) {
+				chainBuilder(firstChain);
+				playerTurn++;
+			} else {chainBuilder(secondChain);
+				playerTurn = 0;}
+			} else {
+				if (playerTurn === 0) {
+					chainBuilder(secondChain);
+					playerTurn = 1;
+				} else {
+					chainBuilder(firstChain); 
+					playerTurn = 0;
+				}
+			}
+		});
+
+	$("#displayMoves").click(function() {
+		if (turn % 2 === 0) {
+			displayMoveChain(firstChain);
+		} else {
+			displayMoveChain(secondChain);
+		}
+	})
 
 
 	chainBuilder = function(chain) {
+		console.log("SHOW YOUR MOVES")
 		$(document).keydown(function(key) {
 			if (chain.length < moveCount) {
 				if (key.keyCode === 37) { 
-					chain.push("left");
+					chain.push("&larr;");
 				} else if (key.keyCode === 38) {
-					chain.push("up");
+					chain.push("&uarr;");
 				} else if (key.keyCode === 39) {
-					chain.push("right");
+					chain.push("&rarr;");
 				} else if (key.keyCode === 40) {
-					chain.push("down");
+					chain.push("&darr;");
 				}
 				console.log("The chain is " + chain)
 			}
-		})
+		});
 	}
 
 	displayMoveChain = function(chain) {
 		chain.forEach(function(element, index) {
 			setTimeout(function(){
+				if (element === "&larr;") {
+					$(".player1Move").attr("class", "throwLeft")
+				}
 				$("#chainDisplay").show().html(element).fadeOut('slow');
 			}, 1000 * (index+1));
 
@@ -51,7 +116,7 @@ $(document).ready(function () {
 			N = firstChain.length}
 			else {N = secondChain.length}
 
-			matchedMoves = 0;
+				matchedMoves = 0;
 			for (var i = 0; i < N; i++) {
 				if (firstChain[i] === secondChain[i]) {
 					matchedMoves++;
@@ -67,13 +132,22 @@ $(document).ready(function () {
 				}
 				moveCount++;
 				turn++;
+				playerTurn++;
 			} else {
 				console.log("Not even close")
+				if (turn % 2 === 0) {
+					player1Score++;
+					$("#p1Score").html(player1Score);
+				} else {
+					player2Score++;
+					$("#p2Score").html(player2Score);
+				}
 				firstChain = [];
 				secondChain = [];
 				turn = 0;
+				playerTurn = 0;
 				moveCount = 3;
 			}
 		}
 
-});
+	});
